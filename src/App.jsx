1fecbf8b98b8876031a222bab5226554eb324648
Mathankumar-93 <mathaneros93@gmail.com>
2023-12-19 11:14:52 +0530

    sample react project

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function App() {
+  let data = [
+    { title: "project 1", description: "hi" },
+    { title: "project 2", description: "hello" },
+    { title: "project 3", description: "how" },
+    { title: "project 4", description: "are" },
+    { title: "project 5", description: "you" },
+    { title: "project 6", description: "mathan" },
+  ];
+  return (
+    <>
+      <div className="container">
+        <h1 className="my-4">
+          Page Heading
+          <small>Secondary Text</small>
+        </h1>
+
+        <div className="row">
+          {data.map((e, i) => {
+            return (
+              <div className="col-lg-4 col-sm-6 mb-4" key={i}>
+                <div className="card h-100">
+                  <a href="#">
+                    <img
+                      className="card-img-top"
+                      src="https://via.placeholder.com/700x400"
+                      alt=""
+                    />
+                  </a>
+                  <div className="card-body">
+                    <h4 className="card-title">
+                      <a href="#">{e.title}</a>
+                    </h4>
+                    <p className="card-text">{e.description}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
